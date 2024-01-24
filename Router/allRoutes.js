b077780c18bd8f0d0069d058773a652216f3e89e
rdrd7 <saveudumonde7@gmail.com>
2024-01-24 19:54:00 +0700

    created card&reservation

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,11 +3,13 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
 new Route("/", "Accueil", "/pages/home.html"),
-new Route("/galerie", "La galerie", "pages/galerie.html"),
+new Route("/galerie", "La galerie", "/pages/galerie.html"),
 new Route("/signin" , "Connexion" , "/pages/auth/signin.html"),
 new Route("/signup" , "Inscription" , "/pages/auth/signup.html"),
 new Route("/account" , "Mon compte" , "/pages/auth/account.html"),
 new Route("/editPassword" , "Changement de mot de passe" , "/pages/auth/editPassword.html"),
+new Route("/card", "La carte", "/pages/card.html"),
+new Route("/reservation", "Les réservations", "/pages/reservation.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
